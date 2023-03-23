3e7a4a5f23c9c892de4cda7348ba9e4ad64211cc
Allan Turton <forshopping50@hotmail.com>
2023-03-23 19:01:47 +1000

    Task 5 done.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "tasks", to: "tasks#index"
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
   get "tasks/:id", to: "tasks#show", as: :task
+
 end
 
 
